35b14b3d57aa094629f7b656defb7dc3b351b8f6
DevDane <dane@danecwalker.com>
2023-01-10 20:39:52 -0500

    Base

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,19 @@
+// Copyright (c) 2023 DevDane <dane@danecwalker.com>
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
